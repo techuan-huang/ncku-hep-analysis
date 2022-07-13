4de5559822dddc2e7a05d18805f0a0054120600f
Te-Chuan Huang <tchuang.phys@gmail.com>
2022-07-13 16:20:34 +0800

    First commit of some cluster settings

--- ClusterSettings/Installation/RockyOpenHPC.sh
+++ ClusterSettings/Installation/RockyOpenHPC.sh
@@ -243,15 +243,17 @@ done
 
 #distribute jobs evenly to compute nodes and start jobs according to job priority
 vim /var/spool/pbs/sched_priv/sched_config
-#job_sort_key: "fairshare_tree_usage LOW"     ALL
-job_sort_key: "cput LOW"        ALL
-node_sort_key: "ncpus LOW assigned"     ALL
+
+smp_cluster_dist: round_robin
+
 fair_share: true        ALL
 unknown_shares: 10
 fairshare_decay_time: 06:00:00
 fairshare_decay_factor: 0.7
 
-qmgr -c "set server job_sort_formula = 2^-(fairshare_tree_usage/fairshare_perc)"
+#qmgr -c "set server job_sort_formula = 2^-(fairshare_tree_usage/fairshare_perc)"
+#qmgr -c "set server job_sort_formula = fairshare_factor"
+qmgr -c "set server job_sort_formula = queue_priority"
 
 
 systemctl restart pbs
