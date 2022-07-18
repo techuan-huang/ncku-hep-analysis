adc58130d6cad9878bae9dbbe7a8fab91dfdc6e4
Te-Chuan Huang <tchuang.phys@gmail.com>
2022-07-18 21:17:12 +0800

    First commit of some cluster settings

--- ClusterSettings/Managing/root/ResyncUsers.sh
+++ ClusterSettings/Managing/root/ResyncUsers.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+echo "Syncing users to compute nodes ..."
 wwsh file resync passwd shadow group
 echo ""
 echo "Done! New user will be seen by compute nodes in few minutes!"
+
+echo "Adding new users to PBS fairshare member ..."
+# temporarily stop the scheduling
+qmgr -c "set sched scheduling = False"
+
+# add users to resource_group for fairshare usage
+cp /var/spool/pbs/sched_priv/resource_group.bak /var/spool/pbs/sched_priv/resource_group
+getent passwd | while IFS=: read -r name password uid gid gecos home shell;
+do
+    if [[ $home =~ ^/home/ ]]; then
+        echo "$name 502 root 10" >> /var/spool/pbs/sched_priv/resource_group
+    fi
+done
+
+# kill -HUP the scheduler to reload the configurations
+kill -HUP $(pgrep -f pbs_sched)
+
+# restart the scheduling
+qmgr -c "set sched scheduling = True"
+
+echo "Done! New user will be prioritized while scheduling!"
