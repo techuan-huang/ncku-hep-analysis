ec5620637caf2e95252dd684a4f638f31372dd02
Te-Chuan Huang <tchuang.phys@gmail.com>
2022-08-07 03:24:30 +0800

    First commit of some cluster settings

--- ClusterSettings/Installation/RockyOpenHPC.sh
+++ ClusterSettings/Installation/RockyOpenHPC.sh
@@ -285,3 +285,10 @@ qmgr -c "set sched scheduling = True"
 systemctl restart pbs
 #or
 kill -HUP $(pgrep -f pbs_sched)
+
+#######################################
+### Network forwarding
+#######################################
+/sbin/iptables -t nat -A POSTROUTING -o eno2 -j MASQUERADE
+/sbin/iptables -A FORWARD -i eno2 -o eno1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -A FORWARD -i eno1 -o eno2 -j ACCEPT
