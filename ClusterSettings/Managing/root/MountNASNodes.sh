465ab7df1eca3726a623fb82b9f7571eb22b0c2f
Te-Chuan Huang <tchuang.phys@gmail.com>
2025-03-01 23:47:42 +0800

    Add new NAS

--- ClusterSettings/Managing/root/MountNASNodes.sh
+++ ClusterSettings/Managing/root/MountNASNodes.sh
@@ -50,3 +50,20 @@ pdsh -w compute-0-3 "mount -t nfs -o rw 192.168.91.19:/volume1/data03 /mnt/data0
 #pdsh -w compute-0-7 "mount -t nfs -o rw 192.168.91.20:/volume1/data03 /mnt/data03"
 #pdsh -w compute-0-8 "mount -t nfs -o rw 192.168.91.21:/volume1/data03 /mnt/data03"
 #pdsh -w compute-0-9 "mount -t nfs -o rw 192.168.91.19:/volume1/data03 /mnt/data03"
+
+#mount NAS (data04) on headnode
+mkdir -vp /mnt/data04
+mount -t nfs -o rw 192.168.91.22:/volume1/data04 /mnt/data04
+
+#mount NAS (data04) on compute nodes
+pdsh -w compute-0-[0-3] "mkdir -vp /mnt/data04"
+pdsh -w compute-0-0 "mount -t nfs -o rw 192.168.91.23:/volume1/data04 /mnt/data04"
+pdsh -w compute-0-1 "mount -t nfs -o rw 192.168.91.24:/volume1/data04 /mnt/data04"
+pdsh -w compute-0-2 "mount -t nfs -o rw 192.168.91.25:/volume1/data04 /mnt/data04"
+pdsh -w compute-0-3 "mount -t nfs -o rw 192.168.91.23:/volume1/data04 /mnt/data04"
+#pdsh -w compute-0-4 "mount -t nfs -o rw 192.168.91.24:/volume1/data04 /mnt/data04"
+#pdsh -w compute-0-5 "mount -t nfs -o rw 192.168.91.25:/volume1/data04 /mnt/data04"
+#pdsh -w compute-0-6 "mount -t nfs -o rw 192.168.91.23:/volume1/data04 /mnt/data04"
+#pdsh -w compute-0-7 "mount -t nfs -o rw 192.168.91.24:/volume1/data04 /mnt/data04"
+#pdsh -w compute-0-8 "mount -t nfs -o rw 192.168.91.25:/volume1/data04 /mnt/data04"
+#pdsh -w compute-0-9 "mount -t nfs -o rw 192.168.91.23:/volume1/data04 /mnt/data04"
